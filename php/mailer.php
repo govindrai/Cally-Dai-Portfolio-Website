10ade7903b93b82380cb90af37e1dc2fa12f5908
Govind Rai <raigovind93@gmail.com>
2017-05-22 07:26:51 +0000

    Add reply to to incoming email and correct sender

--- php/mailer.php
+++ php/mailer.php
@@ -4,13 +4,16 @@
     
     if (array_key_exists('email', $_POST) && array_key_exists('message', $_POST) && array_key_exists('subject', $_POST)) {
     
-        $from = new SendGrid\Email("", $_POST['email']);
+        $from = new SendGrid\Email("CallyDai.com", $_ENV["cally_email"]);
         $subject = $_POST["subject"];
         $to = new SendGrid\Email("Cally Dai", $_ENV["cally_email"]);
-        $content = new SendGrid\Content("text/plain", $_POST["message"]);
+        $content = new SendGrid\Content("text/plain", $_POST["message"] . "\n\n\n-- Sent from CallyDai.com");
         $mail = new SendGrid\Mail($from, $subject, $to, $content);
         $apiKey = $_ENV["sendgrid_api_key"];
         $sg = new \SendGrid($apiKey);
+        $reply_to = new SendGrid\ReplyTo($_POST["email"]);
+        $mail->setReplyTo($reply_to);
+        
         $response = $sg->client->mail()->send()->post($mail);
         // echo $response->statusCode();
         // print_r($response->headers());
